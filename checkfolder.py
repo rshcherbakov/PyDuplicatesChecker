18bab32e15e8b57cda4de1c3a8da55a682cd3c7e
Roman S. Shcherbakov <roman-shcherbakoff@yandex.com>
2019-02-17 15:14:40 +0400

    spark added

--- checkfolder.py
+++ checkfolder.py
@@ -2,10 +2,25 @@ import os
 import sys
 import hashlib
 import filecmp
+import pandas as pd
+import logging
 
-#TODO: get flat list of all files in folder
+from pyspark import SparkContext, SparkConf
+
+os.environ["SPARK_HOME"] = "/usr/local/lib/python3.5/dist-packages/pyspark"
+os.environ["PYSPARK_PYTHON"]="/usr/bin/python3.5"
+
+appName = "PyDuplicatesScaner"
+
+conf = SparkConf()\
+    .setAppName(appName)\
+    .setMaster('local[3]')
+sc = SparkContext(conf=conf)
 
 class FileUnit():
+    def retreaveContent(self):
+        pass
+
     def __init__(self, fname, dirpath):
         path = os.path.join(fname, dirpath)
         self.name = fname
@@ -21,18 +36,58 @@ class CheckDir():
     def __init__(self,path):
         self.path = path
         self.filelist = []
-        self.collect()
-        print(self.filelist)
+        self.df = pd.DataFrame(self.collect(),
+                               columns=["Name", "Path",
+                                        "Size", "Content"])
 
     def collect(self):
-        for subdir, dirs, files in os.walk(self.path):
-            self.filelist += [os.path.join(subdir,fname) for fname in files]
+        def hash(path):
+            if os.path.isfile(path):
+                try:
+                    with open(path, 'rb') as file:
+                        hash = hashlib.sha1(file.read())
+                        res = hash.hexdigest()
+                        logging.info("success %s" % path)
+                        return res
+                except PermissionError:
+                    logging.error("permission error: %s" % path)
+                    return
+                except TypeError:
+                    logging.error("type error: %s" % path)
+                    return
+                except:
+                    logging.error("Unexpected error: % %s" % \
+                                  sys.exc_info()[0])
+                    return
+
+#subdir, dirs, files
+        getthrow = lambda args:\
+            [(fname, os.path.join(args[0],fname))\
+             for fname in args[2]]
+
+        def getInfo(t:tuple):
+            print("try {0}".format(t[1]))
+            size = os.path.getsize(t[1])
+            content = hash(t[1])
+            return (t[0], t[1], size, content)
+
+
+        return sc.parallelize(os.walk(self.path))\
+            .flatMap(getthrow) \
+            .map(getInfo) \
+            .filter(lambda t: len(t) == 4) \
+            .collect()
+
+
+        #for subdir, dirs, files in :
+            #self.filelist += [os.path.join(subdir,fname) for fname in files]
+
 
     def scan(self):
         pass
 
-
-
-
 if __name__ == "__main__":
     dir = CheckDir(os.path.curdir)
+
+    print(dir.df.head(5))
+
