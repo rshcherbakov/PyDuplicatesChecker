36a83985d6c2212cc27dfa98cfa9ae1768b53a42
rshcherbakov <dr.shcherbakoff@gmail.com>
2019-02-17 07:10:13 +0400

    os walk added

--- checkfolder.py
+++ checkfolder.py
@@ -1,18 +1,38 @@
 import os
 import sys
 import hashlib
-
+import filecmp
 
 #TODO: get flat list of all files in folder
 
-class File():
-    def __init__(self, name, path, size, contenthash):
-        self.name = name
-        self.content = contenthash
-        self.size = size
+class FileUnit():
+    def __init__(self, fname, dirpath):
+        path = os.path.join(fname, dirpath)
+        self.name = fname
         self.path = path
-        self
+        self.size = os.path.getsize(path)
 
     def __eq__(self, other):
-        self.name == other.name and \
-            self.size == self.size
+        return self.name == other.name and \
+            self.size == self.size and \
+            filecmp._do_cmp(self.path, other.path)
+
+class CheckDir():
+    def __init__(self,path):
+        self.path = path
+        self.filelist = []
+        self.collect()
+        print(self.filelist)
+
+    def collect(self):
+        for subdir, dirs, files in os.walk(self.path):
+            self.filelist += [os.path.join(subdir,fname) for fname in files]
+
+    def scan(self):
+        pass
+
+
+
+
+if __name__ == "__main__":
+    dir = CheckDir(os.path.curdir)
